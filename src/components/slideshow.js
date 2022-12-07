1c6d2cbcb8a6bbc6c9db738c271aabd119880d1d
Dylan <dylmwrig@gmail.com>
2022-12-06 20:46:23 -0600

    Slideshow works

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -8,29 +8,29 @@ const Slideshow = () => {
   return (
     <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">    
       <ol class="carousel-indicators">
-        <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#homeCarousel" data-slide-to="1"></li>
-        <li data-target="#homeCarousel" data-slide-to="2"></li>
+        <li data-target="#homeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></li>
+        <li data-target="#homeCarousel" data-bs-slide-to="1"></li>
+        <li data-target="#homeCarousel" data-bs-slide-to="2"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src={img2} alt="First slide"></img>
+          <img class="d-block w-100" src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" alt="First slide"></img>
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src={img1} alt="Second slide"></img>
+          <img class="d-block w-100" src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" alt="Second slide"></img>
         </div>
         <div class="carousel-item">
           <img class="d-block w-100" src={img2} alt="Third slide"></img>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#homeCarousel" role="button" data-slide="prev">
+      <button class="carousel-control-prev" data-bs-target="#homeCarousel" data-bs-slide="prev" type="button">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#homeCarousel" role="button" data-slide="next">
+      </button>
+      <button class="carousel-control-next" data-bs-target="#homeCarousel" data-bs-slide="next" type="button">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-      </a>
+      </button>
     </div>
   )
 }

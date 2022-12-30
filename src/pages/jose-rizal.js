d17205b6fee3a64fed3c60d1cf1d4430883537cc
Dylan <dylmwrig@gmail.com>
2022-12-30 16:31:44 -0600

    Volunteer page is mostly in place

--- src/pages/jose-rizal.js
+++ src/pages/jose-rizal.js
@@ -1,7 +1,9 @@
 import * as React from "react"
+
+import rizalImg from "../images/rizal.webp"
+
 import Layout from "../components/layout"
 import {Seo} from "../components/seo"
-import rizalImg from "../images/rizal.webp"
 
 const JoseRizal = () => (
   <Layout>
@@ -12,9 +14,9 @@ const JoseRizal = () => (
             For whom it is named
           </h5>
           <p class="fs-8 fw-light text-muted mt-4"> 
-Purchased in 1974, the building was named in honor of Dr. José P. Rizal, national hero of the Philippines. A patriot, physician, polyglot, and man of letters, his poetry, essays, and novels awakened Philippine identity. He was a supporter of peaceful reform. José P. Rizal is remembered for his brilliance, courage, patriotism, and compassion towards the Filipino people and the country.  He is also remembered for his expressed interest in learning and befriending other peoples and cultures. He lived by example, and showed that nativism is not patriotism. <br/><br/>
-Dr. Rizal's influence is reflected every day through the endeavors of the Filipino American Council of Greater Chicago, in its passion to celebrate Philippine and Philippine American arts and culture and its intersection with diverse ethnicities and cultures, as well as to serve the vulnerable; to fight hunger for low-income seniors and veterans; and to foster the thoughtful, caring, and supportive community that has become the hallmark of the organization. <br/><br/>
-The building was known as the Jose P. Rizal Heritage Center until it was renamed the Jose P. Rizal Community Center ("Rizal Community Center") in 2017. 
+            Purchased in 1974, the building was named in honor of Dr. José P. Rizal, national hero of the Philippines. A patriot, physician, polyglot, and man of letters, his poetry, essays, and novels awakened Philippine identity. He was a supporter of peaceful reform. José P. Rizal is remembered for his brilliance, courage, patriotism, and compassion towards the Filipino people and the country.  He is also remembered for his expressed interest in learning and befriending other peoples and cultures. He lived by example, and showed that nativism is not patriotism. <br/><br/>
+            Dr. Rizal's influence is reflected every day through the endeavors of the Filipino American Council of Greater Chicago, in its passion to celebrate Philippine and Philippine American arts and culture and its intersection with diverse ethnicities and cultures, as well as to serve the vulnerable; to fight hunger for low-income seniors and veterans; and to foster the thoughtful, caring, and supportive community that has become the hallmark of the organization. <br/><br/>
+            The building was known as the Jose P. Rizal Heritage Center until it was renamed the Jose P. Rizal Community Center ("Rizal Community Center") in 2017. 
           </p>
         </div>
         <div className="col-4">
--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -1,52 +1,62 @@
 import * as React from "react"
 
+import volImg from "../images/volunteer.jpg"
+import volImg2 from "../images/volunteer2.jpg"
+
 import "../components/style.scss"
 import Layout from "../components/layout"
 import {Seo} from "../components/seo"
 
 const VolunteerOpportunities = () => (
   <Layout>
-    <div class="container my-5 text-start">
-      <div class="row">
+    <div class="container my-5 ">
+      <img class="text-center" src={volImg2} alt="We love our volunteers!"/>
+      <div class="row volunteer text-start">
         <h5>VOLUNTEERING</h5>
-        <p class="fw-light text-muted">
+        <p> 
 Volunteering is a great way to make a difference and get to know our diverse community. We are always recruiting enthusiastic volunteers, and make the most of your time and skills to accomplish FACC's mission and purposes.<br/>
-Program roles often require the ability to volunteer on a regular schedule: the same day and time each week. We generally ask for a minimum of a six-month commitment when volunteering in a program.<br/><br/>
-In general, all volunteers should be at least 21 years of age and be fluent in English and/or Tagalog.  Volunteers the age of 18 and under must:<br/>
-Have a waiver signed by a parent or guardian
-Be accompanied and actively supervised by an authorized adult.
-Volunteers under the age of 16 must be accompanied and actively supervised by a parent or legal guardian.
-We require one (1) adult chaperone per every ten (10) volunteers age 18 or under.
+Program roles often require the ability to volunteer on a regular schedule: the same day and time each week. We generally ask for a minimum of a six-month commitment when volunteering in a program.<br/>
+        </p>
+        <h5 class="py-2">REQUIREMENTS FOR VOLUNTEERS 21 YEARS AND OLDER:</h5> 
+        <ul>
+          <li>Be fluent in English and/or Tagalog</li>
+          <li>Reside in the greater Chicago area.</li>
+          <li>Commit to a minimum of ten volunteer hours per month.</li>
+          <li>Delight in working with the public.</li>
+          <li>Visit and familiarize themselves with the FACC's policies and procedures and programs in order to answer visitor questions confidently.</li>
+          <li>Attend in-service training.</li><br/>
+        </ul>
+  
+        <h5 class="pb-2">ADDITIONAL REQUIREMENTS FOR VOLUNTEERS 18 YEARS AND YOUNGER:</h5> 
+        <div class="row pb-3">
+          <div class="col-4">
+            <img src={volImg} alt="Boy Scouts Volunteering Opportunity"/>
+          </div>
+          <div class="col-8">
+            <ul> 
+              <li>Have a waiver signed by a parent or guardian</li>
+              <li>Be accompanied and actively supervised by an authorized adult.</li>
+              <li>Volunteers under the age of 16 must be accompanied and actively supervised by a parent or legal guardian.</li>
+              <li>We require one (1) adult chaperone per every ten (10) volunteers age 18 or under.</li>
+            </ul>
+            <p class="text-muted">
+              To discuss youth volunteer opportunities, please contact Sally Velasco at sallyvelascor@yahoo.com.<br/><br/>
+            </p>
+          </div>
+        </div>
 
-
-To discuss youth volunteer opportunities, please contact Sally Velasco at sallyvelascor@yahoo.com 
-
-
-Volunteers must be able to:
-
-Reside in the greater Chicago area.
-Commit to a minimum of ten volunteer hours per month.
-Delight in working with the public.
-Visit and familiarize themselves with the FACC's policies and procedures and programs in order to answer visitor questions confidently.
-Attend in-service training.
-
-
-BENEFITS
-
-Becoming a program volunteer at Rizal Center has many benefits. You’ll be joining a team of volunteers and staff working together to build community and write the next chapter of our organization. Here are some of the benefits volunteering can provide:
-
-
-
-Personal and professional growth: strengthen your existing skills and learn new skills as you volunteer.
-Work in a community center that embraces and reflects the diversity of our community. Meet new people and expand your worldview.
-Make a difference! Every volunteer role is designed to fill a need in our community – your work changes lives.
-
-
- All volunteers are encouraged to become members of the Filipino American Council of Greater Chicago.
-
-
-
-Please review the position descriptions before applying. Listed are some volunteer positions that become available most frequently. 
+        <h5>BENEFITS</h5>
+        <p>
+Becoming a program volunteer at Rizal Center has many benefits. You’ll be joining a team of volunteers and staff working together to build community and write the next chapter of our organization. Here are some of the benefits volunteering can provide:<br/>
+        </p>
+        <ul>
+          <li>Personal and professional growth: strengthen your existing skills and learn new skills as you volunteer.</li>
+          <li>Work in a community center that embraces and reflects the diversity of our community. Meet new people and expand your worldview.</li>
+          <li>Make a difference! Every volunteer role is designed to fill a need in our community – your work changes lives.</li>
+        </ul>
+        <p>
+          All volunteers are encouraged to become members of the Filipino American Council of Greater Chicago.
+          Please review the position descriptions before applying. Listed are some volunteer positions that become available most frequently. 
         </p>
       </div>
     </div>

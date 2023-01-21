2c00ecde0ff9f6dba6833355d1824ab20f5ace62
Dylan <dylmwrig@gmail.com>
2023-01-20 23:03:55 -0600

    The contact form is finished, though I still need to work on style and responsiveness (ie clear form on submission)

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,12 +12,26 @@ const Contact = () => (
     <div class="container my-5 text-start">
       <div class="row">
         <div class="col-5">
-          <h5>CONTACT US</h5>
-          <ContactForm />
+          <h4 class="pb-2">CONTACT US</h4>
+          <p>We are always happy to listen to any of those with questions or feedback for the FACGC and Rizal Community Center.</p>
+          <p class="fw-bold m-0">Filipino American Council of Greater Chicago</p>
+          <p>1332 W. Irving Park Road, Chicago, IL 60613, US
+             sallyvelascor@yahoo.com (1 773-541-1725)</p>
+          <p class="fw-bold">Hours By Appointment</p>
+          <p>
+            Mon   Closed<br/>
+            Tue   12:00 pm - 05:00 pm<br/>
+            Wed   12:00 pm - 05:00 pm<br/>
+            Thu   12:00 pm - 05:00 pm<br/>
+            Fri   12:00 pm - 05:00 pm<br/>
+            Sat   09:00 am - 02:00 pm<br/>
+            Sun   09:00 am - 02:00 pm
+          </p>
         </div>
         <div class="col-1"/>
         <div class="col-6">
-          <p>We are always happy to listen to any of those with questions or feedback for the FACGC and Rizal Community Center.</p>
+          <p>You can also reach us via email by using form and we will get back to you as soon as possible.</p>
+          <ContactForm />
         </div>
       </div>
     </div>

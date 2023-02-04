1b0fdd54d7d787e96849bec07d86f1a37f723dfb
Dylan <dylmwrig@gmail.com>
2023-02-04 17:19:20 -0600

    Worked out that bug

--- src/components/pdf-viewer.js
+++ src/components/pdf-viewer.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import {useState } from 'react'
 
-import pdf from "../newsletter/BayanihanNews2.1.pdf"
+//import pdf from "../newsletter/BayanihanNews2.1.pdf"
 
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -38,32 +38,35 @@ const PDFViewer = (props) => {
   
   return (
     <div>
-      <Document file={pdf}
-        onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNum}/>
-      </Document>
-      <div>
-          <p>
-            Page {pageNum || (numPages ? 1 : '--')} of {numPages || '--'}
-          </p>
-          <button
-            type="button"
-            disabled={pageNum<= 1}
-            onClick={previousPage}
-          >
-            Previous
-          </button>
-          <button
-            type="button"
-            disabled={pageNum>= numPages}
-            onClick={nextPage}
-          >
-            Next
-          </button>
-      </div>
     </div>
   )
 }
+
+      //<Document file={pdf}
+      //  onLoadSuccess={onDocumentLoadSuccess}>
+      //  <Page pageNumber={pageNum}/>
+      //</Document>
+      //<div>
+      //    <p>
+      //      Page {pageNum || (numPages ? 1 : '--')} of {numPages || '--'}
+      //    </p>
+      //    <button
+      //      type="button"
+      //      disabled={pageNum<= 1}
+      //      onClick={previousPage}
+      //    >
+      //      Previous
+      //    </button>
+      //    <button
+      //      type="button"
+      //      disabled={pageNum>= numPages}
+      //      onClick={nextPage}
+      //    >
+      //      Next
+      //    </button>
+      //</div>
+
+
       //onLoadSuccess={onDocumentLoadSuccess}>
 
       //{Array.from(new Array(numPages), (el, index) => (
--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 
-import PDFViewer from "../components/pdf-viewer.js"
+//import PDFViewer from "../components/pdf-viewer.js"
 import "../components/style.scss"
 import Layout from "../components/layout"
 import {Seo} from "../components/seo"
@@ -9,7 +9,6 @@ import {Seo} from "../components/seo"
 const Newsletter = () => (
   <Layout>
     <div id="" class="container my-5"> 
-      <PDFViewer/>
     </div>
   </Layout>
 )

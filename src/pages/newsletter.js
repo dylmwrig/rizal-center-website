3fd1120a8d6d6c2c2570c51b526de2f6dba915da
Dylan <dylmwrig@gmail.com>
2023-02-04 21:01:46 -0600

    Tweaking newsletter page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,9 +1,10 @@
 import * as React from "react"
 import { useState } from 'react'
 
+import PDFViewer from "../components/pdf-viewer.js"
+
 import nl from "../newsletter/BayanihanNews2.1.pdf"
 import nl2 from "../newsletter/sample.pdf"
-import PDFViewer from "../components/pdf-viewer.js"
 
 import "../components/style.scss"
 import Layout from "../components/layout"
@@ -13,12 +14,15 @@ const Newsletter = () => {
   const [pdf, setPDF] = useState(nl)
   return (
   <Layout>
-    <div id="" class="container my-5"> 
+    <div class="container my-5"> 
       <PDFViewer source={pdf}/>
     </div>
     <button onClick={() =>setPDF(nl2)}
       class="btn btn-link"
       type="button">test</button>
+    <button onClick={() =>setPDF(nl)}
+      class="btn btn-link"
+      type="button">goback</button>
   </Layout>
   )
 }

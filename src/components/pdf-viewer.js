1a283617d171ff924dfd8937ca02057d1f3b41b4
Dylan <dylmwrig@gmail.com>
2023-02-19 19:06:52 -0600

    One last try

--- src/components/pdf-viewer.js
+++ src/components/pdf-viewer.js
@@ -5,8 +5,10 @@ import "../components/style.scss"
 
 const PDFViewer = (props) => {
   const pdfSrc = props.source
+    //<iframe src="https://drive.google.com/file/d/120OLilytJ7IduFwCqck7Rg0by8RK32Lw/preview" allow="autoplay"></iframe>
+    //<iframe src="https://drive.google.com/file/d/120OLilytJ7IduFwCqck7Rg0by8RK32Lw/preview" width="640" height="480" allow="autoplay"></iframe>
   return (
-    <iframe id="PDFViewer" src={`${pdfSrc}#view=fitH`}></iframe>
+    <iframe id="PDFViewer" src={`${pdfSrc}#view=fitH`} allow="autoplay"></iframe>
   )
 }
 

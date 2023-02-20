3614c36c40c9c9bb8c4665f7d60af55dfb5c4b91
Dylan <dylmwrig@gmail.com>
2023-02-19 20:33:54 -0600

    Temporarily removing the inquiry news feed link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -34,7 +34,6 @@ const Navbar = ({ siteTitle }) => {
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="/eat-local/"> Eat local </a></li>
                   <li><a class="dropdown-item" href="/newsletter/"> Bayanihan News </a></li>
-                  <li><a class="dropdown-item" href="/news-feed/"> Filipino News </a></li>
                   <li><a class="dropdown-item" href="/celebrating-rizal.js"> Celebrating Rizal Center </a></li>
                 </ul>
             </li>
